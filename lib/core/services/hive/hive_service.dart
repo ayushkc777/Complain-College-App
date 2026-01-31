6e2a266aa24b059f36e911f4b9777af2594f424e
ayushkc777 <ayushkc777@gmail.com>
2026-01-31 07:45:44 +0545

    Sprint 3: Hive ensure init

--- lib/core/services/hive/hive_service.dart
+++ lib/core/services/hive/hive_service.dart
@@ -9,7 +9,7 @@ class HiveService {\n  HiveService._();\n\n  static bool _initialized = false;\n
     await Hive.openBox(sessionBoxName);\n    _initialized = true;\n  }
 
   static Box get _usersBox => Hive.box(usersBoxName);
-  static Box get _sessionBox => Hive.box(sessionBoxName);\n\n  static bool get isReady => _initialized;
+  static Box get _sessionBox => Hive.box(sessionBoxName);\n\n  static bool get isReady => _initialized;\n\n  static Future<void> ensureInitialized() async {\n    if (!_initialized) {\n      await init();\n    }\n  }
 
   static Map<String, dynamic>? getUserByEmail(String email) {
     final key = email.trim().toLowerCase();
@@ -47,6 +47,7 @@ class HiveService {\n  HiveService._();\n\n  static bool _initialized = false;\n
     await _sessionBox.delete('currentUser');
   }
 }
+
 
 
 
