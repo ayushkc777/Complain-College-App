96e6834b4a1625b0be4ff396ad77644ee2e5b0e2
ayushkc777 <ayushkc777@gmail.com>
2026-01-31 18:53:08 +0545

    Sprint 4: API endpoints

---
+++ lib/core/api/api_endpoints.dart
@@ -0,0 +1,10 @@
+class ApiEndpoints {
+  ApiEndpoints._();
+
+  static const String register = '/students';
+  static const String login = '/students/login';
+  static const String batches = '/batches';
+  static const String uploadComplaintPhoto = '/items/upload-photo';
+}
+
+
