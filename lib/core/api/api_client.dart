656cd40e762fb583763388b0165922c323e26161
ayushkc777 <ayushkc777@gmail.com>
2026-01-31 18:52:28 +0545

    Sprint 4: API headers

--- lib/core/api/api_client.dart
+++ lib/core/api/api_client.dart
@@ -1,19 +1,17 @@
 import 'package:dio/dio.dart';
 
-class ApiClient {\n  ApiClient._();\n\n  static const String baseUrl = 'http://10.0.2.2:3000/api/v1';\n  static const int connectTimeoutSeconds = 15;\n  static const int receiveTimeoutSeconds = 20;
+class ApiClient {\n  ApiClient._();\n\n  static const String baseUrl = 'http://10.0.2.2:3000/api/v1';\n  static const int connectTimeoutSeconds = 15;\n  static const int receiveTimeoutSeconds = 20;\n\n  static const Map<String, String> defaultHeaders = {\n    'Content-Type': 'application/json',\n    'Accept': 'application/json',\n  };
 
   static final Dio dio = Dio(
     BaseOptions(
       baseUrl: baseUrl,
       connectTimeout: const Duration(seconds: connectTimeoutSeconds),
       receiveTimeout: const Duration(seconds: receiveTimeoutSeconds),
-      headers: {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json',
-      },
+      headers: defaultHeaders,
     ),
   );
 }
+
 
 
 
